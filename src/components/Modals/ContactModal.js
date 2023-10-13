62b67fda9d9219c883191a75a79cf94189d8eea7
Ridwan Oladipupo <ridwan@spektra.co>
2023-10-13 07:36:53 +0100

    contact form integration

--- src/components/Modals/ContactModal.js
+++ src/components/Modals/ContactModal.js
@@ -7,6 +7,7 @@ import {
   CardBody,
   FormGroup,
   Form,
+  FormFeedback,
   Input,
   InputGroupAddon,
   InputGroupText,
@@ -16,7 +17,33 @@ import {
   Col
 } from "reactstrap";
 
+// Formik Validation
+import * as Yup from "yup";
+import { useFormik } from "formik";
+
 const ContactModal = ({ isOpen, setIsOpen }) => {
+
+    const validation = useFormik({
+        // enableReinitialize : use this flag when initial values needs to be changed
+        enableReinitialize: true,
+    
+        initialValues: {
+          fullName: '',
+          email: '',
+          message: '',
+        },
+        validationSchema: Yup.object({
+            fullName: Yup.string().required("Please Enter Your UserName"),
+            email: Yup.string().required("Please Enter Your Email").email("Invalid email address"),
+            message: Yup.string().required("Please Enter Your Message"),
+        }),
+        onSubmit: (values, {resetForm}) => {
+            // dispatch(createLogin(values))
+            console.log('values', values)
+            resetForm({ values: '' })
+      
+          },
+      });
 
     const handleClose = () => {
         setIsOpen(!isOpen)
@@ -51,7 +78,17 @@ const ContactModal = ({ isOpen, setIsOpen }) => {
                             <Input
                                 placeholder="Enter your full name"
                                 type="text"
+                                name="fullName"
+                                onChange={validation.handleChange}
+                                onBlur={validation.handleBlur}
+                                value={validation.values.fullName || ""}
+                                invalid={
+                                    validation.touched.fullName && validation.errors.fullName ? true : false
+                                }
                             />
+                            {validation.touched.fullName && validation.errors.fullName ? (
+                                <FormFeedback type="invalid">{validation.errors.fullName}</FormFeedback>
+                            ) : null}
                         </FormGroup>
                     </Col>
                     <Col md="6">
@@ -59,7 +96,17 @@ const ContactModal = ({ isOpen, setIsOpen }) => {
                             <Input
                                 placeholder="Enter your email"
                                 type="email"
+                                name="email"
+                                onChange={validation.handleChange}
+                                onBlur={validation.handleBlur}
+                                value={validation.values.email || ""}
+                                invalid={
+                                    validation.touched.email && validation.errors.email ? true : false
+                                }
                             />
+                            {validation.touched.email && validation.errors.email ? (
+                                <FormFeedback type="invalid">{validation.errors.email}</FormFeedback>
+                            ) : null}
                         </FormGroup>
                     </Col>
                 </Row>
@@ -68,12 +115,29 @@ const ContactModal = ({ isOpen, setIsOpen }) => {
                     placeholder="Write your message here ..."
                     rows="5"
                     type="textarea"
+                    name="message"
+                    onChange={validation.handleChange}
+                    onBlur={validation.handleBlur}
+                    value={validation.values.message || ""}
+                    invalid={
+                        validation.touched.message && validation.errors.message ? true : false
+                    }
                 />
+                {validation.touched.message && validation.errors.message ? (
+                        <FormFeedback type="invalid">{validation.errors.message}</FormFeedback>
+                    ) : null}
                 <div className="mt-4">
                     <Button color="danger" type="button" onClick={handleClose}>
                         Cancel
                     </Button>
-                    <Button color="primary" type="button">
+                    <Button 
+                        color="primary" 
+                        type="button" 
+                        onClick={(e) => {
+                            e.preventDefault();
+                            validation.handleSubmit();
+                            return false;
+                        }}>
                         Submit
                     </Button>
                 </div>
